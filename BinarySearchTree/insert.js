7a1ff13ae1a9db9327834fd154dae86405d3d49a
Kannan <kannans02001@gmail.com>
2025-07-31 02:19:39 +0530

    [search operation & inorder traversal of Binary Search Tree]

--- BinarySearchTree/insert.js
+++ BinarySearchTree/insert.js
@@ -36,6 +36,34 @@ class BST {
                }
           }
      }
+
+     contains(value) {
+          let current = this.root;
+          while (current !== null) {
+               if (value === current.value) return true;
+
+               if (value < current.value) {
+                    current = current.left;
+               } else {
+                    current = current.right;
+               }
+          }
+          return false;
+     }
+
+     inOrder() {
+          const result = [];
+
+          function traverse(node) {
+               if (!node) return;
+
+               traverse(node.left);
+               result.push(node.value);
+               traverse(node.right);
+          }
+          traverse(this.root);
+          return result;
+     }
 }
 
 const tree = new BST();
@@ -44,5 +72,11 @@ tree.insert(5);
 tree.insert(15);
 tree.insert(3);
 tree.insert(7);
-tree.insert(12);
+// tree.insert(12);
 tree.insert(18);
+
+console.log(tree.contains(7));
+console.log(tree.contains(12));
+
+
+console.log(tree.inOrder());
