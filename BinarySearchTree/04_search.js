6b7d7baeead2bc801db0fb18d1220b6b3e4a9e7a
Kannan <kannans02001@gmail.com>
2025-08-01 23:01:29 +0530

    Add BST search logic

---
+++ BinarySearchTree/04_search.js
@@ -0,0 +1,65 @@
+class Node {
+     constructor(value) {
+          this.value = value;
+          this.left = null;
+          this.right = null;
+     }
+}
+class BST {
+     constructor() {
+          this.root = null;
+     }
+     insert(value) {
+          const newNode = new Node(value);
+
+          if (this.root === null) {
+               this.root = newNode;
+               return;
+          }
+          let current = this.root;
+
+          while (true) {
+               if (value === current.value) return;
+
+               if (value < current.value) {
+                    if (current.left === null) {
+                         current.left = newNode;
+                         return;
+                    }
+                    current = current.left;
+               } else {
+                    if (current.right === null) {
+                         current.right = newNode;
+                         return;
+                    }
+                    current = current.right;
+               }
+          }
+     }
+
+     contains(value) {
+          let current = this.root;
+          while (current !== null) {
+               if (value === current.value) return true;
+
+               if (value < current.value) {
+                    current = current.left;
+               } else {
+                    current = current.right;
+               }
+          }
+          return false;
+     }
+}
+
+const tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+tree.insert(7);
+// tree.insert(12);
+tree.insert(18);
+
+console.log(tree.contains(7));
+console.log(tree.contains(12));
